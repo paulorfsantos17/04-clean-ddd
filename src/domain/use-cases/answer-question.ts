18e7f9a11eef5b51ad80189a390ab6859dcde5c7
Paulo <paulorfsantos17@gmail.com>
2024-07-11 16:31:34 -0300

    feat: add use case answer-questions

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,23 @@
+import { Answer } from "../entities/answer"
+import { AnswersRepository } from "../repositories/answer-repository"
+
+interface AnswerQuestionUseCaseRequest {
+  questionId: string
+  instrutorId: string,
+  content: string
+}
+
+
+export class AnswerQuestionUseCase {
+  constructor(private answerRepository: AnswersRepository) {
+    this.answerRepository = answerRepository
+  }
+
+  async execute({ instrutorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({ authorId: instrutorId, content, questionId })
+
+    await this.answerRepository.create(answer)
+
+    return answer
+  }
+}

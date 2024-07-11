6068878aad994337de592a6a2a92553c8a19f272
Paulo <paulorfsantos17@gmail.com>
2024-07-11 16:30:06 -0300

    feat: add entities answer, instrutor, student, and answer

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,22 +1,22 @@
 import { randomUUID } from "node:crypto"
 
-interface QuestionProps {
-  title: string,
-  content: string,
-  authorId: string,
+interface AnswerProps {
+  content: string
+  authorId: string
+  questionId: string
 }
 
-export class Question {
+export class Answer {
   public id: string
-  public title: string
   public content: string
   public authorId: string
+  public questionId: string
 
-  constructor(props: Question, id?: string) {
+  constructor(props: AnswerProps, id?: string) {
     this.id = id ?? randomUUID()
-    this.title = props.title
     this.content = props.content
     this.authorId = props.authorId
+    this.questionId = props.questionId
   }
 
 }

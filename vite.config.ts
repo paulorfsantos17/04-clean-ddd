7dae57d6a41558ae1511c31065a020dbd459ad2a
Paulo <paulorfsantos17@gmail.com>
2024-07-13 15:21:50 -0300

    chore: ad vitest coverage  and config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
+    coverage: {
+      provider: 'v8',
+    },
     globals: true,
     include: ['./src/**/*.spec.ts'],
   },
